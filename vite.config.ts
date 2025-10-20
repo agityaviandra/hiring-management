64b0aeff88dca57c1ae4c9f7ea9e420dc7508620
agityaviandra <andra@apta.id>
2025-10-20 12:30:40 +0700

    fix - set vite config for run on macos

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+    preserveSymlinks: false,
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+    force: true, // Force dependency re-optimization
+  },
+  build: { sourcemap: false },
 });
